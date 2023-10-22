8ca300c17393b2b8adc95752a0701cf08eeef5fd
MohammadYounoos <114152824+MohammadYounoos@users.noreply.github.com>
2023-10-22 19:48:27 +0530

    Add files via upload

---
+++ ecommerce/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        frameset {
+            background-color: #f2f2f2;
+        }
+        frame {
+            border: 2px solid #0074b2;
+        }
+    </style>
+</head>
+<frameset rows="15%,5%,*">
+    <frame src="head.html" scrolling="no" style="background-color: #0074b2;" frameborder="0" noresize="noresize">
+    <frame src="link.html" scrolling="no" style="background-color: #005181;" frameborder="0" noresize="noresize">
+    <frame src="buyhome.php" name="data" frameborder="0" noresize="noresize">
+</frameset>
+</html>
